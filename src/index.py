eb75f3fae9fab8b67139edd826750608ceb433c9
MLG_Herobrine <herobrineluver2@gmail.com>
2022-03-31 15:50:36 -0400

    Uploaded offline changes. Currently needs debugging

--- src/index.py
+++ src/index.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 
 # Imports
-
+import tkinter as tk
+from tkinter import ttk
+import json
 # Definitions
+
+def sort_teams(teams):
+	output = {} # Dictionary of teams, sorted by wins. Ex: output['1'] = ['Speaker1-Speaker2', 'Speaker1-Speaker2', 'Speaker1-Speaker2']
+	for k, v in teams.items():
+		# print(k, v)
+		if v['wins'] in output:
+			[output[v['wins']]] = k
+		else: # Better to replace with a else if and throw error if elif doesn't work
+			output[v['wins']].append(k)
+	return output
+
+class App(ttk.Frame):
+	def _init__(self, master=None):
+		super().__init__(master)
+		self.pack()
+
+		# Create widgets with TTK
 
 # Main
 def main():
-	...
+	teams = json.load(open('test_round1.json'))
+	round1 = sort_teams(teams)
+	print(round1)
 
 if __name__ == '__main__': main()
